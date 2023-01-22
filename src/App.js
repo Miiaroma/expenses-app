4c23e7d15d7d2abc0fea33753c6985f3236d7dcf
Miiaroma <miia.romanainen@gmail.com>
2023-01-22 21:34:48 +0200

    ExpenseForm and ExpensesFilter added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -8,7 +9,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Insurance',
@@ -23,12 +24,18 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+  
   return (
     <div>
-      <h2>Let's get started!</h2>
+       <NewExpense onAddExpense={addExpenseHandler} />     
       <Expenses items={expenses}/>
     </div>
   );
 }
 
 export default App;
+
