210f6516167e4fb20303d777bb955faec66717ef
Miiaroma <miia.romanainen@gmail.com>
2023-01-27 13:02:52 +0200

    ExpensesList and conditional content added.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -22,15 +23,8 @@ const Expenses = (props) => {
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
-      />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-        key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
         />
-      ))}
+        <ExpensesList items={filteredExpenses} />
     </Card>
   </div>
   );
